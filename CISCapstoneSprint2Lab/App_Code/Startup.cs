932db4e54805a375f89ddc0767eef38dfcba0eca
LabPatron <labpatron@ad.jmu.edu>
2019-03-25 13:00:35 -0400

    Add project files.

---
+++ CISCapstoneSprint2Lab/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CISCapstoneSprint2Lab.Startup))]
+namespace CISCapstoneSprint2Lab
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
